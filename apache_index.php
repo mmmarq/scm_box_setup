68738fef78c7eff65e8746338a41209b1a403be9
Marcelo Martim Marques <marcelo.martim@gmail.com>
2016-09-30 11:01:54 -0300

    Pass raspberry pi ip address to properly compose service links

--- apache_index.php
+++ apache_index.php
@@ -1,3 +1,4 @@
 <?php
-    header('Location: http://raspberry.esy.es/releases/index.php');
+    $ip = $_SERVER['SERVER_ADDR'];
+    header('Location: http://raspberry.esy.es/releases/index.php?ip='.$ip);
 ?>
